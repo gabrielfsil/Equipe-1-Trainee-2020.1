8d7d9fa174a53bb7dee374456606348de1731c3e
viniman <vinicius.carlos@live.com>
2020-06-12 10:43:54 -0300

    Adiciona arquivos php referentes a navbar e footer público e admin

---
+++ felizlandia/app/views/admin/footer-admin.view.php
@@ -0,0 +1,13 @@
+<div class="spacer"></div>
+  <footer>
+    <div class="informaçoes adm">
+      <div class="barratecnica adm">
+        <img src="img\mappointer-adm.png">
+        2349 Rua Josezinho das nuvens, 203 | (36) 2534-2300 | parquemtlegal@email.com.br <br>
+        Copyright © 2020-2020 Felizlândia mt Todos direitos reservados <br>
+        <a href="index.html">Home</a> |  <a href="quem-somos.html">Quem somos</a> |  <a href="atracoes.html">Atracoes</a> |  <a href="contato.html">Contato</a>
+      </div>
+      <div class="espaço medio"></div>
+    </div>
+  </footer>
+</div>
---
+++ felizlandia/app/views/admin/navbar-admin.view.php
@@ -0,0 +1,32 @@
+<nav id="navbar" class="navbar navbar-expand-md navbar-dark fundofalso sticky" > <!-- bg-transparent-->
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+      <ul class="navbar-nav mr-auto ">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home Administrativa</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Atraçoes</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Categorias</a>
+            </li>
+      </ul>
+    </div>
+    <div class="mx-auto order-0">
+      <a class="navbar-brand mx-auto" href="#"><img src="img/logo.png" class="logo"></a>
+      <span class="TogglerPaginaAtual">Atrações</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Usuários</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
+</nav>
