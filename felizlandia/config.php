f421eec8e6106736f3c8339d648e8ee15c9a8093
viniman <vinicius.carlos@live.com>
2020-06-12 11:05:17 -0300

    Configura conexão ao banco de dados felizlandia_park

--- felizlandia/config.php
+++ felizlandia/config.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'name' => '',
-        'username' => '',
+        'name' => 'felizlandia_park',
+        'username' => 'root',
         'password' => '',
-        'connection' => '',
+        'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
