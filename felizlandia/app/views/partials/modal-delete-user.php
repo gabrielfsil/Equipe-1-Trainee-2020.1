2d5741e2e02813ab31eb029ad76e0ead32d5c8e6
viniman <vinicius.carlos@live.com>
2020-06-12 18:17:30 -0300

    Cria arquivo separado para o modal de remoção de usuário

---
+++ felizlandia/app/views/partials/modal-delete-user.php
@@ -0,0 +1,22 @@
+
+<form action="list-users.html">
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Deseja mesmo excluir este usuário?</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img src="public/img/trash.png" class="img-modal">
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-danger" type="submit" value="delete" name="delete">Confirmar Exclusão</button>
+        </div>
+      </div>
+    </div>
+
+  </div>            
+</form>
