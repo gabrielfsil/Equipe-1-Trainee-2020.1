9cb27b708b92ee082f40a666245a116f2b65e06a
viniman <vinicius.carlos@live.com>
2020-06-13 18:10:48 -0300

    Cria funções de selecionar e inserir dados em uma tabela

--- felizlandia/core/database/QueryBuilder.php
+++ felizlandia/core/database/QueryBuilder.php
@@ -6,25 +6,39 @@ use PDO;
 
 class QueryBuilder
 {
-
     protected $pdo;
 
-
-    public function __construct()
+    public function __construct($pdo)
     {
-    
+        $this->pdo = $pdo;
     }
 
 
-    public function selectAll()
+    public function selectAll($table)
     {
-      
+        $statement = $this->pdo->prepare("select * from {$table}");
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_CLASS);      
     }
 
-    public function insert()
+    public function insert($table, $parameters)
     {
-      
-         
+        $sql = sprintf(
+            'insert into %s (%s) values (%s)',
+            $table,
+            implode(', ', array_keys($parameters)),
+            ':' . implode(', :', array_keys($parameters))
+        );
+
+        try {
+            $statement = $this->pdo->prepare($sql);
+
+            $statement->execute($parameters);
+        } catch (\Exception $e) {
+            //
+        }
     }
     public function edit()
     {
