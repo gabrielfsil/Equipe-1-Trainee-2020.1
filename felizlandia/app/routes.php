bc5644351bdc4df3228ff55baba538775f49d4f6
viniman <vinicius.carlos@live.com>
2020-06-13 11:10:40 -0300

    Cria rotas para as páginas administrativas de usuário

--- felizlandia/app/routes.php
+++ felizlandia/app/routes.php
@@ -1,6 +1,10 @@
  <?php
 
-$router->get('', 'UsersController@home');
+$router->get('admin/list-user', 'UsersController@list');
+$router->get('admin/acess-user', 'UsersController@acess');
+$router->get('admin/delete-user', 'UsersController@delete');
+$router->post('admin/create-user', 'UsersController@create');
+$router->post('admin/edit-user', 'UsersController@edit');
+$router->post('admin/change-pwd-user', 'UsersController@changePassword');
 
-
-?>
+?> 
