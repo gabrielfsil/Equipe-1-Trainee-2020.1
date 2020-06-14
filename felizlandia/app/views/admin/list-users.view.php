00f992a16279b3daf6d73073afe16a03d04a0538
viniman <vinicius.carlos@live.com>
2020-06-14 10:20:29 -0300

    Lista usuários finalizada

--- felizlandia/app/views/admin/list-users.view.php
+++ felizlandia/app/views/admin/list-users.view.php
@@ -2,7 +2,7 @@
 
 <?php require('app/views/partials/navbar-admin.php'); ?>
 
-<div class="adm-users">
+<div class="adm-users"> <!-- body class="adm-users" -->
 
 <div class= "user-list pb-2">
     <div class="container takeoff-scroll">
@@ -11,105 +11,36 @@
       <a href="view-adm-home.html"><button type="button" class="btn btn-primary">&#10094;Voltar</button></a>
       <p><h1>Usuários</h1> </p> 
       <ul class="list-group lista-box">
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Manoel Ferreira Lage</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Julia Aparecida Gonçalves</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Alberto Consagrado de Maria</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Olamira Jacinta Cunha</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Roberta Guilherminda Julemira da Costa</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Júlio Gasparmin Júnio Oliveira</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
-        <li class="list-group-item ">
-          <div class="row justify-content-between">
-                <p>Antônio Cézar Costa</p>
-                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Opções
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="view-user.html">Visualizar</a>
-                  <a class="dropdown-item" href="edit-user.html">Editar</a> 
-                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>
-                </div>
-            </div>
-        </li> 
+        <?php foreach ($users as $user) : ?>
+          <li class="list-group-item ">
+            <div class="row justify-content-between">
+                  <p><?= $user->name; ?> | <?= $user->email; ?></p>
+                  <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Opções
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <form method="POST" action="user-acess">
+                    <input type="hidden" name="id" value="<?= $user->id ?>">         
+                    <button class="dropdown-item" type="submit">Visualizar</button>
+                  </form> 
+                  <form method="POST" action="user-edit">        
+                    <input type="hidden" name="id" value="<?=$user->id?>">         
+                    <button class="dropdown-item" type="submit">Editar</button>
+                  </form>
+                  <form method="POST" action="user-delete">      
+                    <input type="hidden" name="id" value="<?=$user->id?>">         
+                    <button class="dropdown-item" type="submit" data-toggle="modal" data-target="#exampleModal<?= $user->id ?>">Remover</button>
+                  </form>
+                    <!--<a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>-->
+                  </div>
+              </div>
+          </li> 
+        <?php endforeach; ?>
       </ul>
 
       <div class="listar-button mt-2">
-        <a href="create-user.html"> <button class="btn btn-primary button-listar">Adicionar Usuário +</button></a>
+        <a href="user-create"> <button class="btn btn-primary button-listar">Adicionar Usuário +</button></a>
        </div>
-       
     
                   
     </div>
