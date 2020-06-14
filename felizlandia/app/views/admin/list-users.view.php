1cc21072c57ddbaf84d785250cfaac8880c47422
viniman <vinicius.carlos@live.com>
2020-06-14 11:02:16 -0300

    Termina modificações nas views de listar e de remoção

--- felizlandia/app/views/admin/list-users.view.php
+++ felizlandia/app/views/admin/list-users.view.php
@@ -1,55 +1,49 @@
 <?php require('app/views/partials/head.php'); ?>
-
 <?php require('app/views/partials/navbar-admin.php'); ?>
 
-<div class="adm-users"> <!-- body class="adm-users" -->
 
 <div class= "user-list pb-2">
-    <div class="container takeoff-scroll">
-      <!--COLUNA ONDE LISTAMOS Atracoes--> 
-      <!--home ou ja listagem-->
-      <a href="view-adm-home.html"><button type="button" class="btn btn-primary">&#10094;Voltar</button></a>
-      <p><h1>Usuários</h1> </p> 
-      <ul class="list-group lista-box">
-        <?php foreach ($users as $user) : ?>
-          <li class="list-group-item ">
-            <div class="row justify-content-between">
-                  <p><?= $user->name; ?> | <?= $user->email; ?></p>
-                  <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Opções
-                  </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <form method="POST" action="user-acess">
-                    <input type="hidden" name="id" value="<?= $user->id ?>">         
-                    <button class="dropdown-item" type="submit">Visualizar</button>
-                  </form> 
-                  <form method="POST" action="user-edit">        
-                    <input type="hidden" name="id" value="<?=$user->id?>">         
-                    <button class="dropdown-item" type="submit">Editar</button>
-                  </form>
-                  <form method="POST" action="user-delete">      
-                    <input type="hidden" name="id" value="<?=$user->id?>">         
-                    <button class="dropdown-item" type="submit" data-toggle="modal" data-target="#exampleModal<?= $user->id ?>">Remover</button>
-                  </form>
-                    <!--<a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>-->
-                  </div>
-              </div>
-          </li> 
-        <?php endforeach; ?>
-      </ul>
+  <div class="container takeoff-scroll">
+    <!--COLUNA ONDE LISTAMOS Atracoes--> 
+    <!--home ou ja listagem-->
+    <a href="view-adm-home.html"><button type="button" class="btn btn-primary">&#10094;Voltar</button></a>
+    <p><h1>Usuários</h1> </p> 
+    <ul class="list-group lista-box">
+      <?php foreach ($users as $user) : ?>
+        <li class="list-group-item ">
+          <div class="row justify-content-between">
+                <p><?= $user->name; ?></p>
+                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Opções
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <form method="POST" action="user-acess">
+                  <input type="hidden" name="id" value="<?= $user->id ?>">         
+                  <button class="dropdown-item" type="submit">Visualizar</button>
+                </form> 
+                <form method="POST" action="user-edit">        
+                  <input type="hidden" name="id" value="<?=$user->id?>">         
+                  <button class="dropdown-item" type="submit">Editar</button>
+                </form>
+                <form method="POST" action="">      
+                  <input type="hidden" name="id" value="<?=$user->id?>">         
+                  <button class="dropdown-item" type="submit" data-toggle="modal" data-target="#exampleModal<?= $user->id ?>">Remover</button>
+                </form>
+                  <!--<a class="dropdown-item" href="" data-toggle="modal" data-target="#exampleModal">Remover</a>-->
+                </div>
+            </div>
+        </li> 
+      <?php endforeach; ?>
+    </ul>
 
-      <div class="listar-button mt-2">
-        <a href="user-create"> <button class="btn btn-primary button-listar">Adicionar Usuário +</button></a>
-       </div>
-    
-                  
-    </div>
-</div>
+    <div class="listar-button mt-2">
+      <a href="user-create"> <button class="btn btn-primary button-listar">Adicionar Usuário +</button></a>
+      </div>
   
+                
+  </div>
+</div>
+
 
 <?php require('app/views/partials/modal-delete-user.php'); ?>
- 
-</div>
- 
-
 <?php require('app/views/partials/footer-admin.php'); ?>
