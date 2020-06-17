6941b6e6a5f31bc839264c4a92429ff6301af7cc
viniman <vinicius.carlos@live.com>
2020-06-16 22:22:48 -0300

    Arruma problemas com as views, adiciona rotas na navbar e muda modal de senha de usuário

--- felizlandia/app/views/partials/navbar-admin.php
+++ felizlandia/app/views/partials/navbar-admin.php
@@ -2,13 +2,13 @@
     <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
       <ul class="navbar-nav mr-auto ">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home Administrativa</a>
+                <a class="nav-link" href="home">Home Administrativa</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Atraçoes</a>
+                <a class="nav-link" href="list-atracoes">Atraçoes</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="/adm/lista-categorias">Categorias</a>
+                <a class="nav-link" href="lista-categorias">Categorias</a>
             </li>
       </ul>
     </div>
@@ -22,7 +22,7 @@
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Usuários</a>
+          <a class="nav-link" href="/">Usuários</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="#">Logout</a>
