433171ce7a9e61ae993635e7ec0b6baa12ceafcf
viniman <vinicius.carlos@live.com>
2020-06-25 20:04:46 -0300

    Altera links das páginas públicas no footer administrativo, para abrir uma nova aba do navegador

--- felizlandia/app/views/partials/footer-admin.php
+++ felizlandia/app/views/partials/footer-admin.php
@@ -5,7 +5,7 @@
         <img src="\public\img\mappointer-adm.png">
         2349 Rua Josezinho das nuvens, 203 | (36) 2534-2300 | parquemtlegal@email.com.br <br>
         Copyright © 2020-2020 Felizlândia mt Todos direitos reservados <br>
-        <a href="/">Home</a> |  <a href="/quem_somos">Quem somos</a> |  <a href="/atracoes">Atrações</a> |  <a href="/contato">Contato</a>
+        <a href="/" target="_blank">Home</a> |  <a href="/quem_somos" target="_blank">Quem somos</a> |  <a href="/atracoes" target="_blank">Atrações</a> |  <a href="/contato" target="_blank">Contato</a>
       </div>
     </div>
   </footer>
