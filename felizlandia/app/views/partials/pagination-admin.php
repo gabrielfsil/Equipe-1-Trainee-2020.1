b1c470dc2664f500d4a00259c34f0b411816aed8
viniman <vinicius.carlos@live.com>
2020-06-28 00:21:59 -0300

    Modifica para não mostrar setas na primeira e ultima pagina do paginagion

--- felizlandia/app/views/partials/pagination-admin.php
+++ felizlandia/app/views/partials/pagination-admin.php
@@ -3,7 +3,7 @@
     <div class="container listar-pagination">
         <nav class="pagination-listar ">
            <ul class="pagination">
-           
+           <?php if($page!=1): ?>
            <li class="page-item">
               <form method="GET">
                 <input type='hidden' value="1" name="pagination">
@@ -17,6 +17,7 @@
                 <button type="submit" class="page-link text-dark"> < </button>
               </form>
             </li>
+            <?php endif; ?>
 
             <?php for($y=$page-1;$y<$page-1+$link_per_page && $y <= $total_links; $y++) : ?>
 
@@ -35,7 +36,7 @@
       <?php endfor; ?>
 
       
-
+      <?php if($page!=$total_links): ?>                
             <li class="page-item">
               <form method="GET">
                 <input type='hidden' value="<?= $page==$total_links ? $total_links : $page+1 ?>" name="pagination">
@@ -48,7 +49,7 @@
                 <button type="submit" class="page-link text-dark"> >> </button>
               </form>
             </li>
-
+      <?php endif; ?>
             
            </ul>
         </nav>
